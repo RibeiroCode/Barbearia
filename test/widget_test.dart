9a03d29b32b6e011c06018a1af8ef2e82d721f73
RibeiroCode <59852018+RibeiroCode@users.noreply.github.com>
2020-06-30 16:19:42 -0300

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:barber_test/main.dart';
+import 'package:imovelapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
